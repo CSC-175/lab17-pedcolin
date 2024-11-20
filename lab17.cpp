9da958ba6ef2f4845c8ad5dfddfccf43a4a1b074
Colin Pedersen <pedcolin@umich.edu>
2024-11-19 22:42:49 -0500

    attempt 1

--- lab17.cpp
+++ lab17.cpp
@@ -1,1 +1,13 @@
 // Definition of findHigh function goes here
+#include <iostream>
+using namespace std;
+template <typename A, typename B>
+void findHigh(A array[], int size, int &index, B &highnum) {
+    highnum=array[0];
+    for (int i=0; i <size; i++) {
+        if (array[i] > highnum) {
+            index=i;
+            highnum = array[i];
+        }
+    }
+}
